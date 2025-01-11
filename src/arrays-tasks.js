4b09b1d310dfd6e6e2d4e6184d3e4a2041de705a
Muhammadamin1308 <sharifjanovmukhammadamin@gmail.com>
2025-01-11 23:12:38 +0500

    init: start

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -13,15 +13,15 @@
  * @param {number} start - The first number of an array.
  * @param {number} end - The last number of an array.
  * @return {array} - An array of integers.
- *
+ *x`
  * @example
  *    getIntervalArray(1, 5)  => [ 1, 2, 3, 4, 5 ]
  *    getIntervalArray(-2, 2)  => [ -2, -1, 0, 1, 2 ]
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return Array.from({ length: end - start + 1 }, (_, i) => start + i);
 }
 
 /**
@@ -37,8 +37,11 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const maxLength = Math.max(arr1.length, arr2.length);
+  return Array.from({ length: maxLength }, (_, i) =>
+    arr1[i] && arr2[i] ? arr1[i] + arr2[i] : arr1[i] || arr2[i]
+  );
 }
 
 /**
